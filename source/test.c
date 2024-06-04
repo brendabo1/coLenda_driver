296e067157d6c413a806f268d3473473d9f69556
Maike de Oliveira Nascimento <64764136+DestinyWolf@users.noreply.github.com>
2024-06-04 13:43:27 -0300

    feat: update teste file, add car animation

--- source/test.c
+++ source/test.c
@@ -1,40 +1,50 @@
 #include <stdio.h>
 #include <unistd.h>
-#include "colenda.c"
+#include "colenda.h"
 
 
 
 int main() {
 
+    struct timespec req = {0, 500000000};
+
     GPU_open();
 
     Sprite sp;
-    sp.coord_x = 100;
-    sp.coord_y = 100;
+    sp.coord_x = 150;
+    sp.coord_y = 460;
     sp.data_register = 2;
     sp.visibility = 1;
-    sp.offset = BLUE_BLOCK;
+    sp.offset = PURPLE_CAR;
 
     Polygon pol;
     pol.color.red = 7;
     pol.color.blue = 7;
     pol.color.green = 7;
-    pol.coord_x = 300;
-    pol.coord_y = 200;
+    pol.coord_x = 150;
     pol.mem_address = 2;
     pol.shape = 0;
     pol.size = 1;
 
     Color color;
 
-    color.red = 7;
-    color.blue = 0;
-    color.green = 0;
+    color.red = 2;
+    color.blue = 7;
+    color.green = 5;
+
+    set_background_color(color);
 
     set_sprite(sp);
+
+    while(1){
+        nanosleep(&req, NULL);
+        sp.coord_y -= 20;
+        set_sprite(sp);
+        if(sp.coord_y <=20){
+            sp.coord_y = 480;
+        }
+    }
 
     GPU_close();
     return 0;
 }
-
-// 1'111'111'111'0001'011001000'100101100'0010'0011
