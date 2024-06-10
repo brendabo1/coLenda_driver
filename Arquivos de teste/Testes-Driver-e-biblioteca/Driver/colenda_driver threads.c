9c4d3d682969874b9ce3ed345bb0d08f65a4aa72
Camila Boa Morte <116687830+camilaqPereira@users.noreply.github.com>
2024-06-10 19:25:13 -0300

    refactor: move  driver test file to another directory

--- Arquivos de teste/Testes-Driver-e-biblioteca/Driver/colenda_driver threads.c
+++ Arquivos de teste/Testes-Driver-e-biblioteca/Driver/colenda_driver threads.c
@@ -10,7 +10,7 @@
 /* Meta information*/
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Brenda Barbosa, Camila Boa Morte, Maike de Oliveira");
-MODULE_DESCRIPTION("Um driver para realizar a comunicação com o processador gráfico CoLenda na FPGA");
+MODULE_DESCRIPTION("Um driver para realizar a comunicação com o processador gráfico na FPGA");
 
 /* Barramentos do processador gráfico */
 #define DATAA 0x80
@@ -29,7 +29,14 @@ MODULE_DESCRIPTION("Um driver para realizar a comunicação com o processador gr
 /*Nome do driver */
 #define DRIVER_NAME "colenda_driver"
 
-#define BUFF_SIZE 65 
+#define BUFF_SIZE 8
+
+/* Definição da(s) thread(s) */
+static struct task_struct *kthread_buffer_manager;
+
+/* Definição de mutexes */
+DEFINE_MUTEX(read_mutex);
+DEFINE_MUTEX(write_mutex);
 
 /*Driver data*/
 static struct
@@ -37,6 +44,7 @@ static struct
   dev_t devnum;
   struct cdev cdev;
   size_t buffer_size;
+  atomic_t counter; //numero de processos que estão conectados ao driver
   void *LW_virtual;
   volatile int *data_a;
   volatile int *data_b;
@@ -47,13 +55,34 @@ static struct
   
 } colenda_driver_data;
 
+/* Task realizada pela thread */
+int manage_instruction_buffer(void *data){
+
+
+
+  return 0;
+}
+
+
 static int colenda_driver_open(struct inode *device_file, struct file *instance){
   pr_info("%s: open was called!\n", DRIVER_NAME);
+
+  if(atomic_inc_return(&colenda_driver_data.counter) == 1){
+    wake_up_process(kthread_buffer_manager);
+    pr_info("%s: kthread buffer manager is running now!", DRIVER_NAME);
+  }
+
   return 0;
 }
 
 static int colenda_driver_close(struct inode *device_file, struct file *instance){
   pr_info("%s: close was called!\n", DRIVER_NAME);
+
+  if(atomic_dec_and_test(&led_driver_data.counter)){
+    kthread_stop(kthread_buffer_manager);
+    pr_info("%s: kthread buffer manager stopped!", DRIVER_NAME); //fix bug with completion
+  }
+
   return 0;
 }
 
@@ -61,8 +90,6 @@ static ssize_t colenda_driver_write(struct file *file, const char __user *buffer
 loff_t *ppos){
   char kbuffer[colenda_driver_data.buffer_size];
   ssize_t size = min(sizeof(kbuffer), count); 
-  long long int value = 0;
-  int i = 0;
 
   /* Atualizando offset passado pelo usuario*/
   *ppos = 0;
@@ -70,6 +97,9 @@ loff_t *ppos){
   if(size == 0){ //sem bytes a serem escritos
     return 0;
   }
+
+  *colenda_driver_data.wr_reg = 0;
+  
 
   if (!*colenda_driver_data.screen){
     printk("tela em renderização\n");
@@ -86,24 +116,14 @@ loff_t *ppos){
     printk("fila cheia\n");
     return -1;
   }
-
-  printk("%s\n", kbuffer);
-
-  /* Convertendo string em inteiro*/ 
-  for(i = 7; i>=0; i--){
-    value |= kbuffer[i] << ( 8 * i);
-  }
-
-  /*String não pode ser convertida*/
-  if(value == -1){
-    pr_err("%s: fail to converto buffer to int", DRIVER_NAME);
-    return -1;
-  }
+  //printk("%lld\n", kbuffer);
 
   
-  *colenda_driver_data.data_a = (value & 0xFFFFFFFF);
-  *colenda_driver_data.data_b = (value >> 32);
-    
+  *colenda_driver_data.data_a = (kbuffer[4]) << 24 | (kbuffer[5]) << 16 | (kbuffer[6]) << 8 | (kbuffer[7]);
+  *colenda_driver_data.data_b = (kbuffer[0]) << 24 | (kbuffer[1]) << 16 | (kbuffer[2]) << 8 | (kbuffer[3]);
+  
+
+
 
   //Envia sinal para escrita na fila
   *colenda_driver_data.wr_reg = 1;
@@ -139,6 +159,9 @@ static int __init colenda_driver_init(void){
   /*Registrando dispositivo de caractere*/
   cdev_init(&colenda_driver_data.cdev, &fops);
 
+  /* Inicializando contador de processos */
+  atomic_set(&led_driver_data.counter, 0);
+
   /*Mapeando lightweight HPS-to-FPGA brigde*/
   colenda_driver_data.LW_virtual = ioremap(LW_BRIDGE_BASE, LW_BRIDGE_SPAN);
   
@@ -149,6 +172,20 @@ static int __init colenda_driver_init(void){
   colenda_driver_data.wr_full = (int*) (colenda_driver_data.LW_virtual + WRFULL);
   colenda_driver_data.screen = (int*) (colenda_driver_data.LW_virtual + SCREEN);
   colenda_driver_data.reset_pulsecounter = (int*) (colenda_driver_data.LW_virtual + RESET_PULSECOUNTER);
+
+  /* Criando thread de gerenciamento de instruções */
+  kthread_buffer_manager = kthread_create(manage_instruction_buffer, &t1, "instruction_kthread");
+
+  /* Criando kthread de gerenciamento do buffer de instruções */
+  if (kthread_buffer_manager != NULL)
+  {
+    pr_info("%s: kthread buffer manager created!", DRIVER_NAME);
+  }else{
+    pr_err("%s: failed to create kthread buffer manager!", DRIVER_NAME); //refactor this part with go to
+    iounmap(colenda_driver_data.LW_virtual);
+    unregister_chrdev_region(colenda_driver_data.devnum, 1);
+    return -1; //change to a error code
+  }
 
   /* Adicionando dispositivo de caractere */
   result = cdev_add(&colenda_driver_data.cdev, colenda_driver_data.devnum, 1);
